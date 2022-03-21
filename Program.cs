db7719414e18c4274c62fb0c91b5adbe82247ea3
David Brockwell <dbrockwell@my.wctc.edu>
2022-03-21 13:55:00 -0500

    created the search with count and movie display

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
 
 namespace MediaLibrarySearch
 {
@@ -77,7 +78,16 @@ namespace MediaLibrarySearch
                         Console.WriteLine(m.Display());
                     }
                 } else if(choice == "3") {
+                    Console.WriteLine("Enter a movie title or part of one");
+                    string search = Console.ReadLine();
+                    int movieCount = movieFile.Movies.Where(m => m.title.Contains(search)).Count();
+                    Console.WriteLine($"There are {movieCount} movies with \"{search}\" in the title");
 
+                    var movieSearch = movieFile.Movies.Where(m => m.title.Contains(search));
+                    foreach(Movie m in movieSearch)
+                    {
+                        Console.WriteLine(m.Display());
+                    }
                 }
             } while (choice == "1" || choice == "2" || choice == "3");
 
